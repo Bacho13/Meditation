28ede1d5d13b045077ea0a88627773f0195a8ad3
bacho <bacholashkhia13@gmail.com>
2024-09-17 23:13:57 +0400

    add affirm. texts

--- app/(tabs)/affirmations/[itemId].tsx
+++ app/(tabs)/affirmations/[itemId].tsx
@@ -1,35 +1,79 @@
-import { View, Text } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { useLocalSearchParams } from 'expo-router'
-import { GalleryPreviewData } from '@/constants/models/AffirmationCategory';
-import AFFIRMATION_GALLERY from '@/constants/affirmation-gallery';
-
-const AffirmationPractice = () => {
-    const {itemId} = useLocalSearchParams();
-
-    const [affirmation,setAffirmation] = useState<GalleryPreviewData>()
-    
-    useEffect(()=>{
-        for(let idx = 0; idx < AFFIRMATION_GALLERY.length; idx++) {
-            const affirmationData = AFFIRMATION_GALLERY[idx].data
-
-            const affirmationToStart = affirmationData.find((a) => a.id === Number(itemId));
-  
-            if (affirmationToStart) {
-                setAffirmation(affirmationToStart);
-
-                return
-            }
-
-        }
-        
-    }, [])
-
-  return (
-    <View className='flex-1 bg-slate-700 justify-center'>
-      <Text className='text-center justify-center text-red-200 text-4xl'>{itemId}</Text>
-    </View>
-  )
+import {
+  View,
+  Text,
+  ImageBackground,
+  Pressable,
+  ScrollView,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { router, useLocalSearchParams } from "expo-router";
+import { GalleryPreviewData } from "@/constants/models/AffirmationCategory";
+import AFFIRMATION_GALLERY from "@/constants/affirmation-gallery";
+import AppGradient from "@/components/AppGradient";
+import { AntDesign } from "@expo/vector-icons";
+{
 }
 
-export default AffirmationPractice
+const AffirmationPractice = () => {
+  const { itemId } = useLocalSearchParams();
+
+  const [affirmation, setAffirmation] = useState<GalleryPreviewData>();
+  const [sentences, setSentences] = useState<string[]>([]);
+
+  useEffect(() => {
+    for (let idx = 0; idx < AFFIRMATION_GALLERY.length; idx++) {
+      const affirmationData = AFFIRMATION_GALLERY[idx].data;
+
+      const affirmationToStart = affirmationData.find(
+        (a) => a.id === Number(itemId)
+      );
+
+      if (affirmationToStart) {
+        setAffirmation(affirmationToStart);
+
+        const affirmationArray = affirmationToStart.text.split(".");
+   
+        if(affirmationArray[affirmationArray.length - 1] === '' ) {
+            affirmationArray.pop();
+        }
+
+        setSentences(affirmationArray);
+
+
+
+        return;
+      }
+    }
+  }, []);
+
+  return (
+    <View className="flex-1 bg-slate-700 justify-center">
+      <ImageBackground
+        source={affirmation?.image}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <AppGradient colors={["rgba(0,0,0,0.3)", "rgba(0,0,0,0.9)"]}>
+          <Pressable
+            onPress={() => router.back()}
+            className="absolute top-16 left-6 z-10"
+          >
+            <AntDesign name="leftcircleo" size={50} color="white" />
+          </Pressable>
+          <ScrollView className="mt-40  showsVerticalScrollIndicator={false}">
+            <View className="h-full justify-center">
+              <View className="h-4/5 justify-center">
+              {sentences?.map((sentences, idx) => (
+
+                <Text key={idx} className="text-white text-3xl mb-12 font-bold text-center ">{sentences}.</Text>
+              ))}
+              </View>
+            </View>
+          </ScrollView>
+        </AppGradient>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default AffirmationPractice;
